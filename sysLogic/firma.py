131e6f82bba42193a8eaa89b31174b2281731125
K4speeer <dri.taha24@gmail.com>
2023-05-19 22:25:28 +0300

    updated firma.py by defining digital pin to variable

--- sysLogic/firma.py
+++ sysLogic/firma.py
@@ -1,14 +1,15 @@
 from pyfirmata import ArduinoNano
-import time
-import json
+
 
 
 if __name__ == "__main__":
-    board = ArduinoNano("/dev/ttyUSB0", baudrate=9600 , timeout=1)
+    board = ArduinoNano("/dev/ttyUSB0", baudrate=9600)
     print("communication successfully started !!!")
 
+    led = board.digital[13]
+
     while True:
-        board.digital[13].write(1)
-        time.sleep(4)
-        board.digital[13].write(0)
-        time.sleep(2)
+        led.write(1)
+        board.pass_time(1)
+        led.write(0)
+        board.pass_time(1)
