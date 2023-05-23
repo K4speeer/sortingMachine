92089778b395871b97356933d04691d81c7d99ab
K4speeer <dri.taha24@gmail.com>
2023-05-23 18:25:59 +0300

    Reformatting firma.py from using Pyfirmata library to Pymata4

--- sysLogic/firma.py
+++ sysLogic/firma.py
@@ -1,17 +1,24 @@
 # For running this script should use dgtEnv enviroment because of used python version 
-from pyfirmata import ArduinoNano
+from pymata4 import pymata4
+import time 
+import asyncio
 
+board = pymata4.Pymata4()
 
+board.set_pin_mode_digital_output(13)
+board.get_analog_map()
+board.get_capability_report()
+board.get_firmware_version()
+board.get_protocol_version()
+board.get_pymata_version()
 
-if __name__ == "__main__":
-    board = ArduinoNano("/dev/ttyUSB0", baudrate=9600)
-    print("communication successfully started !!!")
+try:
+    while True:
+        board.digital_write(13,1)
+        time.sleep(2)
+        board.digital_write(13,0)
+        time.sleep(1)
 
-    #Defining IR sensor port 
-    # ir_sensor = board.digital[]
-
-    #Defining PCA-9685 pins to connect with arduino
-
-    #Defining Stepper motor shield pins
-
-    
+except KeyboardInterrupt:
+    board.shutdown()
+    exit()
