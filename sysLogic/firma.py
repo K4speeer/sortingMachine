98f36ccb13ddc2ca09f7fb0ced4cd987f978ff4c
K4speeer <dri.taha24@gmail.com>
2023-05-26 03:04:33 +0300

    IR-sensor connected, value changes are readable

--- sysLogic/firma.py
+++ sysLogic/firma.py
@@ -1,25 +1,54 @@
-'''
-The projects logic is:
-    1. Intro IR detects a ball 
-    2. Start feeding the Cassette, using intro lilServo
-'''
 from pymata4 import pymata4
 import time 
 import asyncio
 
+
+
+ir = 7 
+innser_led = 13
+
+
+
+def blink(value, board, pin):
+    if value == 0:
+        board.digital_write(pin, 1)
+        time.sleep(.5)
+        board.digital_write(pin, 0)
+        time.sleep(.5)
+    else:
+        pass
+
+def ir_listener(board, pin):
+    def check_state(value):
+        if value == 0:
+            print("zero")
+        else:
+            print("one")
+    
+    try:
+        while True:
+            value, time_stamp = board.digital_read(ir)
+            check_state(value)
+            time.sleep(.5)
+    except KeyboardInterrupt:
+        board.shutdown()
+        exit()
+
+
 board = pymata4.Pymata4()
 
-board.set_pin_mode_digital_output(13)
-board.set_pin_mode_digital_input(7)
+board.set_pin_mode_digital_output(innser_led)
+board.set_pin_mode_digital_input(ir)
 
 
 try:
     while True:
-        board.digital_write(13,1)
-        time.sleep(2)
-        board.digital_write(13,0)
-        time.sleep(1)
-
+        ir_listener(board, ir)
 except KeyboardInterrupt:
     board.shutdown()
     exit()
+
+    
+
+
+
